5f7734707c524f8f23b47b783bbaf1be652833de
Rasmus Lystrøm <rasmusl@microsoft.com>
2024-03-22 04:45:40 +0100

    Naming

---
+++ modules/policies/scripts/Compare-Definition.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $ManagementGroupId
+)
+
+function Write-Compare ([Parameter(Mandatory = $true)][String]$Label, $Object, $SideIndicator, $Prefix) {
+    $compare = $Object | Where-Object SideIndicator -eq $SideIndicator | ForEach-Object { "$Prefix $($PSItem.InputObject)" }
+    if ($compare) {
+        Write-Output $Label
+        Write-Output $compare
+    }
+}
+
+$source = Get-ChildItem -Path "$PSScriptRoot/../definitions" -Filter "*.json" |
+ForEach-Object {
+    $template = $PSItem | Get-Content | ConvertFrom-Json
+    $template.name
+}
+
+$cloud = Get-AzPolicyDefinition -ManagementGroupName $ManagementGroupId -Custom |
+Where-Object ResourceId -Match "^/providers/Microsoft.Management/managementGroups/$ManagementGroupId/" |
+Select-Object -ExpandProperty Name
+
+$compare = Compare-Object -ReferenceObject ($Cloud ?? @()) -DifferenceObject ($Source ?? @()) -IncludeEqual
+Write-Compare -Label "Definitions to be created:" -Object $compare -SideIndicator "=>" -Prefix "+"
+Write-Compare -Label "Definitions to be updated:" -Object $compare -SideIndicator "==" -Prefix "*"
+Write-Compare -Label "Definitions to be deleted:" -Object $compare -SideIndicator "<=" -Prefix "-"
+
+if ($compare | Where-Object SideIndicator -EQ "<=") {
+    Write-Error "Delete detected. Manual intervention required."
+}
--- modules/policies/scripts/Test-Definition.Tests.ps1
+++ modules/policies/scripts/Test-Definition.Tests.ps1
@@ -1,16 +1,16 @@
 BeforeDiscovery {
     $path = Resolve-Path "$PSScriptRoot/../definitions"
 
-    $script:Policies = Get-ChildItem -Path "$path/*.json"
+    $script:Definitions = Get-ChildItem -Path "$path/*.json"
 }
 
-Describe "Test-Policy" {
-    It "<PSItem.Name> name is same as file name" -ForEach $script:Policies {
+Describe "Test-Definitions" {
+    It "<PSItem.Name> name is same as file name" -ForEach $script:Definitions {
         $template = Get-Content -Path $PSItem | ConvertFrom-Json
         $template.name | Should -BeExactly $PSItem.BaseName
     }
 
-    It "<PSItem.Name> is a policy definition" -ForEach $script:Policies {
+    It "<PSItem.Name> is a policy definition" -ForEach $script:Definitions {
         $template = Get-Content -Path $PSItem | ConvertFrom-Json
         $template.type | Should -Be "Microsoft.Authorization/policyDefinitions"
     }
